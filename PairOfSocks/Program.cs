1c51876a90bad6e4e2e70c1dc8c0ab467c67fa13
Ewerton Willams da Silva <ewerton.willams@philips.com>
2020-09-30 22:33:15 -0300

    Problem solved: PairOfSocks

---
+++ PairOfSocks/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PairOfSocks
+{
+    class Program
+    {
+        static int SockMerchant(int n, int[] ar)
+        {
+
+            Dictionary<int, int> socksCount = new Dictionary<int, int>();
+            int socksPairs = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (!socksCount.ContainsKey(ar[i]))
+                {
+                    socksCount[ar[i]] = 1;
+                }
+                else
+                {
+                    int currentCount = socksCount[ar[i]];
+                    currentCount++;
+                    socksCount[ar[i]] = currentCount;
+                    if (currentCount % 2 == 0)
+                    {
+                        socksPairs++;
+                    }
+                }
+            }
+            
+            return socksPairs;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] socks = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            SockMerchant(9, socks);
+            Console.WriteLine(SockMerchant(9, socks));
+        }
+    }
+}
